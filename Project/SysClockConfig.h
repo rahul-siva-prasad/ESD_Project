2f6a60bdbebe7ed4b95fec9d9c9b48b7367e3f3a
Rahul Prasad <47021075+rahul-siva-prasad@users.noreply.github.com>
2022-12-12 10:18:20 +0530

    Delete Project directory

--- Project/SysClockConfig.h
+++
@@ -1,2 +0,0 @@
-#include "stm32f407xx.h"
-void SysClockConfig (void);
--- Project/UART_Config.h
+++
@@ -1,4 +0,0 @@
-#include"SysClockConfig.h"
-void UART_config(void);
-void UART2_SendChar(uint8_t c);
-void UART2_SendString(char *string);
